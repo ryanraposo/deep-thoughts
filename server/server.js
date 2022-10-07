77a802431211cc541fcc1fa8513ad490135ed0c4
Ryan Raposo <raposo.ryan@gmail.com>
2022-10-06 20:09:52 -0400

    add mutations

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require ('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const db = require('./config/connection');
 
@@ -9,7 +10,8 @@ const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 const app = express();
