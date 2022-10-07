ee3b06ac753d5dfd517fe873aea56a6d3804d33a
Ryan Raposo <raposo.ryan@gmail.com>
2022-10-06 21:04:10 -0400

    integrate client

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2020 by Lernantino</div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ client/src/pages/SingleThought.js
@@ -0,0 +1,19 @@
+const SingleThought = props => {
+  return (
+    <div>
+      <div className="card mb-3">
+        <p className="card-header">
+          <span style={{ fontWeight: 700 }} className="text-light">
+            Username
+          </span>{' '}
+          thought on createdAt
+        </p>
+        <div className="card-body">
+          <p>Thought Text</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SingleThought;
